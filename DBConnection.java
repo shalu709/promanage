0f85759288bd783e88f379111765ba467a451e85
Shalu Kushwaha <kushwahashalu709@gmail.com>
2026-02-23 11:01:38 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,16 +1,27 @@
-package com.promanage;
+package com.promnage.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
 
     private static final String URL =
             "jdbc:postgresql://localhost:5432/promanage";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "shalu2606";
 
-    public static Connection getConnection() throws Exception {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "shalu2606"; 
+
+    public static Connection getConnection() {
+
+        try {
+            System.out.println("Connecting to database...");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (SQLException e) {
+            System.out.println("Database connection failed");
+            e.printStackTrace();
+            return null;
+        }
     }
 }
