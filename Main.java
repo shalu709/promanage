ad892016b68b7c206217d61afc4c7eec64b8a4f4
Shalu Kushwaha <kushwahashalu709@gmail.com>
2026-02-23 11:06:02 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,56 @@
-package com.promnage.main;
+package com.promanage;
 
-import com.promnage.model.Project;
-import com.promnage.service.*;
+import java.util.*;
 
-import java.util.List;
+public class Main {
 
-public class PromnageSystem {
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ProjectDAO dao = new ProjectDAO();
 
-        try {
-            System.out.println("Connecting to database...");
+        while (true) {
 
-            List<Integer> history = DataService.getLast3WeekRevenue();
+            System.out.println("\n1. Add Project");
+            System.out.println("2. View Projects");
+            System.out.println("3. Generate Schedule");
+            System.out.println("4. Exit");
 
-            double predictedRevenue = WeightedMovingAveragePredictor.predict(history);
+            int choice = sc.nextInt();
 
-            System.out.println("Predicted Next Week Revenue = " + predictedRevenue);
+            switch (choice) {
 
-            List<Project> projects = DataService.getAllProjects();
+                case 1:
+                    sc.nextLine();
 
-            Scheduler.scheduleProjects(projects, predictedRevenue);
+                    System.out.print("Title: ");
+                    String title = sc.nextLine();
 
-        }
-        catch (Exception e) {
-            e.printStackTrace();
+                    System.out.print("Deadline (1-5): ");
+                    int deadline = sc.nextInt();
+
+                    System.out.print("Revenue: ");
+                    int revenue = sc.nextInt();
+
+                    dao.addProject(new Project(title, deadline, revenue));
+                    break;
+
+                case 2:
+                    List<Project> list = dao.getAllProjects();
+                    list.forEach(p ->
+                            System.out.println(p.getId() + " | "
+                                    + p.getTitle() + " | "
+                                    + p.getDeadline() + " | "
+                                    + p.getRevenue()));
+                    break;
+
+                case 3:
+                    List<Project> projects = dao.getAllProjects();
+                    Scheduler.generateSchedule(projects);
+                    break;
+
+                case 4:
+                    System.exit(0);
+            }
         }
     }
